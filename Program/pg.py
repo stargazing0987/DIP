ed8ba028a35938faa445134ddbeb2f11936ebc23
Zhang Wenbo <126996447+stargazing0987@users.noreply.github.com>
2024-12-07 01:10:13 +0800

    program

---
+++ Program/pg.py
@@ -0,0 +1,99 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# import some libraries about rational bezier curve
+
+def WindingNumberCurve(Gamma,Query_point):
+    # This function is to calculate the winding number of a curve Gamma around a point Query_point
+    # Input:
+    # Gamma: the curve, a 2D numpy array, each row is a point on the curve
+    # Query_point: the point, a 2D numpy array, each row is a point
+    # Output:
+    # wn: the winding number of Gamma around Query_point
+    return wn
+def CreateSecant(Gamma):
+    # This function is to create the secant of a curve Gamma
+    # Input:
+    # Gamma: the curve, a 2D numpy array, each row is a point on the curve
+    # Output:
+    # Secant: the secant of Gamma
+    return Secant
+    
+def IsInBoundingbox(Gamma,Query_point):
+    # This function is to check whether a point is in the bounding box of a curve
+    # Input:
+    # Gamma: the curve, a 2D numpy array, each row is a point on the curve
+    # Query_point: the point, a 2D numpy array, each row is a point
+    # Output:
+    # True or False
+    return True
+
+def IntegerWindingNumberCurve(Gamma,Query_point):
+    # This function is to calculate the integer winding number of a curve Gamma around a point Query_point
+    # Input:
+    # Gamma: the curve, a 2D numpy array, each row is a point on the curve
+    # Query_point: the point, a 2D numpy array, each row is a point
+    # Output:
+    # wn: the integer winding number of Gamma around Query_point
+    return wn
+
+def isSimpleConvex(Gamma):
+    # This function is to check whether a curve is simple convex
+    # Input:
+    # Gamma: the curve, a 2D numpy array, each row is a point on the curve
+    # Output:
+    # True or False
+    return True
+
+def ConvexEndpointWindingNumber(Gamma,Query_point):
+    # This function is to calculate the winding number of a curve Gamma around a point Query_point
+    # Input:
+    # Gamma: the curve, a 2D numpy array, each row is a point on the curve
+    # Query_point: the point, a 2D numpy array, each row is a point
+    # Output:
+    # wn: the winding number of Gamma around Query_point
+    return wn
+
+def ISInControlPolygon(Gamma,Query_point):
+    # This function is to check whether a point is in the control polygon of a curve
+    # Input:
+    # Gamma: the curve, a 2D numpy array, each row is a point on the curve
+    # Query_point: the point, a 2D numpy array, each row is a point
+    # Output:
+    # True or False
+    return True
+
+def  IsApproximatelyLinear(Gamma,Epsilon):
+    # This function is to check whether a curve is approximately linear
+    # Input:
+    # Gamma: the curve, a 2D numpy array, each row is a point on the curve
+    # Epsilon: the error bound
+    # Output:
+    # True or False
+    return True
+
+def  PolygonWindingNumber(Polygon,Query_point):
+    # This function is to calculate the winding number of a polygon around a point Query_point
+    # Input:
+    # Polygon: the polygon, a 2D numpy array, each row is a point on the polygon
+    # Query_point: the point, a 2D numpy array, each row is a point
+    # Output:
+    # wn: the winding number of Polygon around Query_point
+    return wn
+
+def IsOnSameSide(Point1,Point2,Line):
+    # This function is to check whether two points are on the same side of a line
+    # Input:
+    # Point1: the first point, a 2D numpy array, each row is a point
+    # Point2: the second point, a 2D numpy array, each row is a point
+    # Line: the line, a 2D numpy array, each row is a point
+    # Output:
+    # True or False
+    return True
+
+#main function
+
+
+
+
